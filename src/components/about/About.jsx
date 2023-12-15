cb88e28edb739b3482d0dac11d2aca263ec6bd74
GustavoS7 <gustavods2003@outlook.com>
2023-12-15 00:32:00 -0300

    dsadsapfjpsa

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,14 +1,36 @@
+import { Icon } from "./icon/Icon";
 
 export function About() {
   return (
     <div className="about__container" id="about">
       <div className="about__content">
+      <div className="about__img">
+          <Icon />
+        </div>
         <div className="about__info">
           <h1 className="about__title">Sobre <span>mim</span></h1>
 
-          <p className="about__me">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero minus itaque animi commodi esse delectus fugit! Quasi, velit! Eveniet vel accusamus perferendis ullam aspernatur odit magnam unde nobis sequi dolores.
-          </p>
+          <div className="about__me">
+            <p>Olá, prazer! Meu nome é Gustavo. </p>
+
+            <p>Eu sou um desenvolvedor fullstack, nascido em 2003 no Brasil.</p>
+
+            <p>Desde o começo de minha carreria com dev,</p>
+
+            <p>venho colaborando com a construção de um mundo virtual, em uma imobiliaria chamada Edmur Imóveis</p>
+
+            <p>Trabalhando como desenvolvedor front-end e back-end.</p>
+
+            <p>Sou naturalmente confiante e curioso, </p>
+
+            <p>buscando desafios e melhorar minhas habilidades.</p>
+
+            <p>Uso o codar como uma forma de liberdade, </p>
+
+            <p>em explorar as infinitas de possibilidades.</p>
+
+            <p>Vamos construir algo juntos?</p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,17 +1,51 @@
 import { Typescript } from './icons/typescript'
 import { Javascript } from './icons/javascript'
+import { Html } from './icons/html'
+import { Css } from './icons/css'
+import { Bootstrap } from './icons/Bootstrap'
+import { Docker } from './icons/Docker'
+import { Git } from './icons/Git'
+import { Github } from './icons/Github'
+import { Jest } from './icons/jest'
+import { Mysql } from './icons/mysql'
+import { NextJS } from './icons/next'
+import { NodeJS } from './icons/node'
+import { Npm } from './icons/npm'
+import { Php } from './icons/php'
+import { PostgreSql } from './icons/postgre'
+import { React } from './icons/react'
+import { Tailwind } from './icons/tailwind'
+import { Visual } from './icons/visual'
+import { Yarn } from './icons/yarn'
 
 export function Skills() {
   return (
     <div className="skills__container">
       <div className="skills__content">
-        <h1>
+        <h1 className='skills_title'>
           Minhas <span>Habilidades</span>
         </h1>
 
         <div className="skills__techs">
           <Typescript />
           <Javascript />
+          <Html />
+          <Css />
+          <Bootstrap />
+          <Docker />
+          <Git />
+          <Github />
+          <Jest />
+          <Mysql />
+          <NextJS />
+          <NodeJS />
+          <Npm />
+          <Php />
+          <PostgreSql />
+          <React />
+          <Tailwind />
+          <Visual />
+          <Yarn />
         </div>
       </div>
     </div>
